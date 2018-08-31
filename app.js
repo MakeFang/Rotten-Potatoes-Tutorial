42d10ae0ccbaf59c02ebac48b8112811852f3718
Kuan-Ying Fang <kuanyingfang@gmail.com>
2018-08-31 11:16:36 -0700

    Init

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+var exphbs = require('express-handlebars');
+
+const app = express()
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+  res.render('home',{ msg: 'Hello World!'});
+})
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000!')
+})
