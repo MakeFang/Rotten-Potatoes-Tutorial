aa323e8521dc4b9ed5e2eb099690ec1ed8596e1e
Kuan-Ying Fang <kuanyingfang@gmail.com>
2018-09-02 12:51:14 -0700

    Added vanilla bootstrap

--- app.js
+++ app.js
@@ -12,39 +12,5 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(methodOverride('_method'));
 
-const reviews = require('./controller/reviews.js')(app);
-
-/*
-let reviews = [
-  { title: "Great Review" },
-  { title: "Next Review" }
-]
-*/
-
-/*
-const Review = mongoose.model('Review', {
-    title: String,
-    description: String,
-    movieTitle: String
-});
-*/
-
-//import reviews from 'reviews';
-
-/*
-app.get('/', (req, res) => {
-  res.render('home',{ msg: 'Hello World!'});
-})
-*/
-
-/*
-app.get('/', (req, res) => {
-  Review.find()
-    .then((reviews) => {
-      res.render('reviews-index', { reviews: reviews });
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-})
-*/
+const reviews = require('./controller/reviews.js');
+reviews(app);
