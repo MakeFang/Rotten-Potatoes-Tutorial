ab35ba052709ff325940189eb46005c0edc532c3
Kuan-Ying Fang <kuanyingfang@gmail.com>
2018-09-01 19:57:20 -0700

    Users can see single reviews

--- app.js
+++ app.js
@@ -40,15 +40,25 @@ app.get('/', (req, res) => {
 
 app.post('/reviews', (req, res) => {
   Review.create(req.body).then((review) => {
-    console.log(review);
-    res.redirect('/');
+    console.log(review)
+    res.redirect(`/reviews/${review._id}`)
   }).catch((err) => {
-    console.log(err.message);
+    console.log(err.message)
   })
 })
 
+//how to resolve new/:id conflict?
+
 app.get('/reviews/new', (req, res) => {
   res.render('reviews-new', {});
+})
+
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then((review) => {
+    res.render('reviews-show', { review: review })
+  }).catch((err) => {
+    console.log(err.message);
+  })
 })
 
 app.listen(3000, () => {
